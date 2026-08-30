84e68ae526216ad0f5ab98089c091083e20b5278
Kevin-N0 <126273157+Kevin-N0@users.noreply.github.com>
2026-08-30 14:48:39 -0400

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.py
@@ -0,0 +1,44 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 1. BF
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
+
+        # 2. Sorting 
+        # A = []
+        # for i, num in enumerate(nums):
+        #     A.append([num, i])
+        # A.sort()
+        # l, r = 0, len(nums) - 1
+        # while l < r: 
+        #     l_val, l_idx = A[l]
+        #     r_val, r_idx = A[r]
+        #     cur = l_val + r_val
+        #     if cur == target:
+        #         return [min(l_idx, r_idx), max(l_idx, r_idx)]
+        #     if cur < target:
+        #         l += 1
+        #     else: 
+        #         r -= 1
+        # return []
+
+        # 3. Hash Map (two pass)
+        indices = {}
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i: 
+                return [i, indices[diff]]
+        return []
+
+
+
+            
+
+        

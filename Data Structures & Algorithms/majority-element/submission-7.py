d2ffb7901e72f9a5e3d226bccbbb99c611d4983e
Kevin-N0 <126273157+Kevin-N0@users.noreply.github.com>
2026-09-04 19:01:59 -0400

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # 1. Brute Force, O(), O()
+        # n = len(nums)
+        # for num in nums:
+        #     count = 0 
+        #     for val in nums:
+        #         if val == num:
+        #             count += 1
+        #     if count > n // 2:
+        #         return num
+
+        n = len(nums)
+        for num in nums:
+            count = sum(1 for val in nums if val == num)
+            if count > n // 2:
+                return num
+
+
+
+
+            
+
+        # 2. Hash Map, O(), O()
+
+        # 3. Sorting, O(), O()
+
+        # 4. Bit Manipulation, O(), O()
+
+
+
+
+
+
+
+
+

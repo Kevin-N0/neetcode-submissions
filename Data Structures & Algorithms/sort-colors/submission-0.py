e083afb88e54fd3ad7edad9d87c40dad559542d3
Kevin-N0 <126273157+Kevin-N0@users.noreply.github.com>
2026-09-05 18:18:25 -0400

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 1. Brute Force, O(N log N), O(1) | O(N) depending on sorting algo
+        nums.sort()
+
+
+        # 2. Counting Sort, O(), O()
+
+        # 3. Three Pointers - I, O(), O()
+
+        # 4. Three Pointers - II, O(), O()
+
+        # 5. Three Pointers - III, O(), O()
